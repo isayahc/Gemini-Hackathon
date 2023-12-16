75974aea0cbdf74794fd10754fbd463e1e9406f5
isayhc <isayahculbertson@gmail.com>
2023-12-16 11:19:08 -0500

    allow for uploading of multiple files

--- app.py
+++ app.py
@@ -18,19 +18,26 @@ if "messages" not in st.session_state:
 
 # File upload feature in the sidebar
 # The file uploader is placed in the sidebar so it remains accessible
-uploaded_file = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
-if uploaded_file is not None:
-    # Save the uploaded file to a temporary location
-    with NamedTemporaryFile(delete=False, suffix=os.path.splitext(uploaded_file.name)[1]) as tmp_file:
-        tmp_file.write(uploaded_file.getvalue())
-        uploaded_file_path = tmp_file.name
-    # Add the uploaded file path to the chat history
-    st.session_state.messages.append({"role": "user", "content": f"Uploaded image: {uploaded_file_path}"})
-    # Display the uploaded image in the sidebar
-    st.sidebar.image(uploaded_file, caption="Uploaded Image")
-    # Send a message to the chatbot indicating an image has been uploaded
-    with st.chat_message("user"):
-        st.markdown(f"Uploaded image: {uploaded_file_path}")
+uploaded_files:list = st.sidebar.file_uploader(
+    "Upload an image",
+    type=["png", "jpg", "jpeg"],
+    accept_multiple_files=True,
+    )
+
+# Check if any files have been uploaded
+if uploaded_files:
+    for uploaded_file in uploaded_files:
+        # Save the uploaded file to a temporary location
+        with NamedTemporaryFile(delete=False, suffix=os.path.splitext(uploaded_file.name)[1]) as tmp_file:
+            tmp_file.write(uploaded_file.read())  # Use .read() to read the content of the uploaded file
+            uploaded_file_path = tmp_file.name
+        # Add the uploaded file path to the chat history
+        st.session_state.messages.append({"role": "user", "content": f"Uploaded image: {uploaded_file_path}"})
+        # Display the uploaded image in the sidebar
+        st.sidebar.image(uploaded_file, caption=uploaded_file.name)
+        # Send a message to the chatbot indicating an image has been uploaded
+        with st.chat_message("user"):
+            st.markdown(f"Uploaded image: {uploaded_file.name}")
 
 # Display previous messages in the chat history
 for message in st.session_state.messages:
