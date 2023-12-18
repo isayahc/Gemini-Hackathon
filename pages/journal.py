f3888abea49fdb66c15d711857e85ed93a7c18aa
isayhc <isayahculbertson@gmail.com>
2023-12-17 20:01:03 -0500

    updated interface

--- pages/journal.py
+++ pages/journal.py
@@ -19,46 +19,49 @@ st.title('My Journal')
 # Load existing data
 data = load_data()
 
-# Reset form flag
-reset = st.session_state.get('reset', False)
-
-# User input for new journal entry
+# Form for new journal entry
 with st.form("new_entry", clear_on_submit=True):
     entry_date = st.date_input("Date", datetime.date.today())
-    entry_text = st.text_area("Journal Entry", help="You can use Markdown formatting here.", key='entry_text')
+    entry_text = st.text_area("Journal Entry", help="You can use Markdown formatting here.")
     submit_button = st.form_submit_button("Submit")
 
     if submit_button:
         new_entry = pd.DataFrame({'Date': [entry_date], 'Entry': [entry_text]})
         data = pd.concat([data, new_entry], ignore_index=True)
         save_data(data)
-        st.session_state['reset'] = True
 
-if reset:
-    st.session_state['entry_text'] = ''
-    st.session_state['reset'] = False
-
-# Edit and Delete functionality
-selected_entry_index = st.selectbox("Select an entry to edit or delete", data.index, format_func=lambda x: data.iloc[x]['Date'])
-if st.button("Delete Entry"):
-    data = data.drop(selected_entry_index)
-    save_data(data)
-
-with st.form("edit_entry"):
-    edit_date = st.date_input("Edit Date", value=pd.to_datetime(data.iloc[selected_entry_index]['Date']))
-    edit_text = st.text_area("Edit Journal Entry", value=data.iloc[selected_entry_index]['Entry'])
-    edit_submit = st.form_submit_button("Save Changes")
-
-    if edit_submit:
-        data.at[selected_entry_index, 'Date'] = edit_date
-        data.at[selected_entry_index, 'Entry'] = edit_text
-        save_data(data)
-
-# Display past entries
+# Display past entries with edit/delete options
 st.write("## Past Entries")
-for _, row in data.iterrows():
-    st.write(f"### {row['Date']}")
-    st.markdown(row['Entry'], unsafe_allow_html=True)
-    st.markdown("---")
+for index, row in data.iterrows():
+    cols = st.columns([0.8, 0.1, 0.1])
+    with cols[0]:
+        st.markdown(f"### {row['Date']}")
+        st.markdown(row['Entry'], unsafe_allow_html=True)
+
+    if cols[1].button("Edit", key=f"edit{index}"):
+        st.session_state['edit_index'] = index
+        st.session_state['edit_date'] = row['Date']
+        st.session_state['edit_entry'] = row['Entry']
+
+    if cols[2].button("Delete", key=f"del{index}"):
+        data = data.drop(index)
+        save_data(data)
+        st.experimental_rerun()
+
+# Edit entry
+if 'edit_index' in st.session_state:
+    with st.form("edit_entry"):
+        edit_date = st.date_input("Edit Date", value=pd.to_datetime(st.session_state['edit_date']))
+        edit_text = st.text_area("Edit Journal Entry", value=st.session_state['edit_entry'])
+        save_edit = st.form_submit_button("Save Changes")
+
+        if save_edit:
+            data.at[st.session_state['edit_index'], 'Date'] = edit_date
+            data.at[st.session_state['edit_index'], 'Entry'] = edit_text
+            save_data(data)
+            del st.session_state['edit_index']  # Remove the index from the session state
+            st.experimental_rerun()
+
+st.markdown("---")
 
 # Run the app: streamlit run your_script.py
